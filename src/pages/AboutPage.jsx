f43aab1facb9d30c8f714e91d323d7676e0d808d
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-06-30 17:22:01 -0400

    Section 5 - Links to AboutPage and back to Home

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/shared/Card";
+import { Link } from 'react-router-dom';
 
 function AboutPage() {
   return (
@@ -7,9 +8,9 @@ function AboutPage() {
             <h1>About this project</h1>
             <p>This is a React App to leave feedback for a product or service.</p>
             <p>Version 1.0.0</p>
-            
+
             <p>
-                <a href="/">Back To Home</a>
+                <Link to="/">Back To Home</Link>
             </p>
         </div>
     </Card>
